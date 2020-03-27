4c653cd1ac5ed7aa6122523603885f3752e5e4f5
Kevin Schultz <cisengineer2@gmail.com>
2020-03-27 16:23:51 -0700

    NPM Scripts Part 1

---
+++ js/myscripts.js
@@ -0,0 +1,31 @@
+// used to enable tooltips through bootstrap (Currently disabled) 
+/*  $(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+}) */
+
+// jquery controls for carousel verson 1 (Currently Disabled) 
+/*$(function() {
+    $(".carousel").carousel( { interval: 2000 } );
+    $("#carouselPause").click(function(){
+	$(".carousel").carousel("pause");
+    });
+    $("#carouselPlay").click(function(){
+	$(".carousel").carousel("cycle");
+    });
+});*/
+
+// jQuery Play Pause Toggle Single Button
+$(function() {
+    $(".carousel").carousel( { interval: 2000 } );
+    $("#carouselButton").click(function(){
+	if ($("#carouselButton").children("i").hasClass("fa-pause")) {
+	    $(".carousel").carousel("pause");
+	    $("#carouselButton").children("i").removeClass("fa-pause");
+	    $("#carouselButton").children("i").addClass("fa-play");
+	} else {
+	    $(".carousel").carousel("cycle");
+	    $("#carouselButton").children("i").removeClass("fa-play");
+	    $("#carouselButton").children("i").addClass("fa-pause"); 
+	}
+    });
+});
